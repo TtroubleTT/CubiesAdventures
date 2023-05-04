a00c497fc1a525f43181abd58df4ba7af97129c8
TtroubleTT <TtroubleTT2@gmail.com>
2023-05-03 23:57:40 -0700

    ui/score
    added ui and score

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+
+    public TMP_Text scoreText;
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = $"{(float) Math.Round(player.position.z, 0)}";
+    }
+}
